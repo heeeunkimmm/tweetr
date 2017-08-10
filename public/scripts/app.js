136204bed640662d497cc1378b69aeb2b6062c2f
Hannah Kim <h.heeeun.kim@gmail.com>
2017-08-10 19:47:21 +0000

    added tweetload feature and finished tweets input feature

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,82 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from tweets.json
+$(function () {
+
+function renderTweets(tweets) {
+  $(".tweets").empty();                     //emptying "tweets" class
+  tweets.forEach(function (tweet) {         //going through each tweets submitted
+    var aTweet = createTweetElement(tweet); //creating aTweet via createTweetElement function
+    $(".tweets").prepend(aTweet);           //posting aTweet (new Tweet) under "tweets" html class
+  });
+}
+
+//creating new tweet according to html format below
+function createTweetElement(tweet) {
+    return `
+        <article class="tweet module">
+          <header class="module-header">
+            <img class="tweet-avatar" src=${tweet.user.avatars.small}>
+            <span class="tweet-name module-title">${tweet.user.name}</span>
+            <span class="tweet-handle">${tweet.user.handle}</span>
+          </header>
+          <main class="module-body">
+            <div class="tweet-text">${tweet.content.text}</div>
+          </main>
+          <footer class="module-footer tweet-meta">
+            <span class="tweet-timestamp">1 hour ago</span>
+            <span class="tweet-actions">
+              <a href="#" class="tweet-action">Like</a>
+              <a href="#" class="tweet-action">Report</a>
+              <a href="#" class="tweet-action">Retweet</a>
+            </span>
+          </footer>
+        </article>
+    `;
+};
+
+// fetching tweets from /tweets page
+function loadTweets() {
+  $.getJSON('/tweets', renderTweets)          //handling JSON response from '/tweets' and rendering it to the page
+}
+
+$("form").submit(function (event) {
+    event.preventDefault();                   //stopping defalut submit button that refreshes the page
+
+    let input = $(this).find('textarea').val().length;     //finding 'this' form's textarea's length
+
+    if (input === 0) {
+      alert("uh oh, you didn't tweet anything! Please try again");
+      return
+    };
+
+    if (input > 140) {
+      alert("uh oh, your tweet is too long!");
+      return
+    }
+
+    const form = this;    const counter = $(this).closest('form').find('.counter');
+
+
+    $.ajax({                                  //using ajax to post new tweet from the data it gets from /tweets
+      url: '/tweets',
+      method: 'post',
+      data: $(form).serialize()               //turning data into query string that gets included to POST request body
+    }).done(function () {
+      form.reset();                           //after its done, the form resets.
+      $(".counter").text(140);                //after submit, counter goes back to 140
+      loadTweets();                           //after submit button is pressed, it loads the tweets to the page.
+    });
+
+  });
+
+  loadTweets();   //after submit button is pressed, it loads the tweets to the page.
+
+});
+
+
+
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,20 +4,31 @@
 // });
 
 $(function() {
-  var maxCount = 140;
+  // var maxCount = 140;
 
-  $("textarea").on("keypress", function(event) {
-    maxCount--;
-    console.log(maxCount)
-  });
+  // $("textarea").on("keypress", function(event) {
+  //   maxCount--;
+  //   console.log(maxCount)
+  // });
 
-  $("textarea").on("keydown", function(event) {
-    if (event.key === "Backspace") {
-      maxCount++;
-      console.log(maxCount);
+  // $("textarea").on("keydown", function(event) {
+  //   if (event.key === "Backspace") {
+  //     maxCount++;
+  //     console.log(maxCount);
+  //   }
+  // });
+
+
+  $('.new-tweet').find('textarea').on('keyup', function() {
+    const charsLeft = 140 - $(this).val().length;
+    const counter = $(this).closest('form').find('.counter');
+    counter.text(charsLeft);
+    if (charsLeft < 0) {
+      counter.addClass('red');
+    } else {
+      counter.removeClass('red');
     }
   });
-
 
 
 });
